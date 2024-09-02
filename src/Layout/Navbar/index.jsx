4893a676421664167d026bd2137dc4a7e2bd8a99
Gasarayev <gasarayev.emil@gmail.com>
2024-09-02 15:23:27 +0400

    navbar mobile

--- src/Layout/Navbar/index.jsx
+++ src/Layout/Navbar/index.jsx
@@ -10,43 +10,68 @@ import Logo from "../../assets/img/logoNavbar.png";
 
 function NavbarMenu() {
   return (
-    <Navbar bg="dark" data-bs-theme="dark">
-      <Container className="d-flex justify-content-between align-center">
-        <div href="#home" className="logo">
-          <img src={Logo} alt="logo" />
-        </div>
-        <Nav className="gap-3">
-          <Nav.Link className="menu_items" href="#home">
-            Home
-          </Nav.Link>
-          <Nav.Link className="menu_items" href="#features">
-            About
-          </Nav.Link>
-          <Nav.Link className="menu_items" href="#pricing">
-            Product
-          </Nav.Link>
-          <Nav.Link className="menu_items" href="#pricing">
-            Contact
-          </Nav.Link>
-        </Nav>
-        <Nav className="d-flex align-items-center">
-          <NavDropdown
-            title={<span className="custom-nav-title">EN</span>}
-            id="navbarScrollingDropdown"
-          >
-            <NavDropdown.Item href="#">AZ</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#">RU</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="#">
-            <FaInstagram />
-          </Nav.Link>
-          <Nav.Link href="#">
-            <FaFacebookF />
-          </Nav.Link>
-        </Nav>
-      </Container>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="pe-2 ps-2">
+      <div href="#home" className="logo logo_desktop">
+        <img src={Logo} alt="logo" />
+      </div>
+
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        
+        id="responsive-navbar-nav"
+      >
+        <Container className="mobile_version d-flex justify-content-between align-center">
+          <div href="#home" className="logo logo_mobile">
+            <img src={Logo} alt="logo" />
+          </div>
+          <Nav className="mobile_menu_list gap-3">
+            <Nav.Link className="menu_items" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="menu_items" href="#features">
+              About
+            </Nav.Link>
+            <Nav.Link className="menu_items" href="#pricing">
+              Product
+            </Nav.Link>
+           
+            <Nav.Link className="menu_items" href="#pricing">
+              Contact
+            </Nav.Link>
+
+            <NavDropdown
+            className="lang_mobile"
+              title={<span className="custom-nav-title">English</span>}
+              id="navbarScrollingDropdown"
+            >
+              <NavDropdown.Item href="#">Azerbaijan</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">Russian</NavDropdown.Item>
+            </NavDropdown>
+           
+          </Nav>
+
+          <Nav className="icon_mobile_version d-flex align-items-center justify-content-center">
+            <NavDropdown
+            className="lang_desc_version"
+              title={<span className="custom-nav-title">EN</span>}
+              id="navbarScrollingDropdown"
+            >
+              <NavDropdown.Item href="#">AZ</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">RU</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">
+              <FaInstagram />
+            </Nav.Link>
+            <Nav.Link href="#">
+              <FaFacebookF />
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar.Collapse>
     </Navbar>
+
   );
 }
 
